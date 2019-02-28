95cd72a8bf9305e974d58f4c9fa01566f7c2a9ac
MichaelDuo <michaeldong21@gmail.com>
2019-02-28 14:46:47 +0800

    Separate debug-ui

---
+++ debug-ui/App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Mindmap from '../src/index';
+
+class App extends Component {
+    public render(): JSX.Element {
+        return (
+            <div className="App">
+                <Mindmap />
+            </div>
+        );
+    }
+}
+
+export default App;
