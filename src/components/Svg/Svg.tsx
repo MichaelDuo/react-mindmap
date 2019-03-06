e8a323a1a5271696e4eed764fd77c5c86da17700
MichaelDuo <michaeldong21@gmail.com>
2019-03-06 09:36:51 +0800

    Add svgjs

--- src/components/Svg/Svg.tsx
+++ src/components/Svg/Svg.tsx
@@ -1,7 +1,22 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, RefObject } from 'react';
+import SVG from 'svg.js';
 
 export default class Editor extends PureComponent<{}> {
+    public svgEl: RefObject<HTMLDivElement>;
+
+    public constructor(props: {}) {
+        super(props);
+        this.svgEl = React.createRef();
+    }
+
+    public componentDidMount(): void {
+        if (this.svgEl.current) {
+            let draw = SVG(this.svgEl.current).size(300, 300);
+            draw.rect(100, 100).attr({ fill: '#f06' });
+        }
+    }
+
     public render(): JSX.Element {
-        return <div>{this.props.children}</div>;
+        return <div ref={this.svgEl}>{this.props.children}</div>;
     }
 }
