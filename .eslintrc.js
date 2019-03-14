80a010e9dc87d453c9c486bba49f060021c10d5d
MichaelDuo <michaeldong21@gmail.com>
2019-03-14 11:16:04 +0800

    Update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,5 +8,7 @@ module.exports = {
     plugins: ['@typescript-eslint', 'react'],
     rules: {
         'no-undef': 0, // https://github.com/eslint/typescript-eslint-parser/issues/437#issuecomment-435526531
+        '@typescript-eslint/explicit-function-return-type': 0,
+        '@typescript-eslint/no-empty-interface': 0,
     },
 };
