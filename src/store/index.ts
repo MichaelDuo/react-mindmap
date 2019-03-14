b938e26ee61af044fa9e637f54a700cf47d12530
MichaelDuo <michaeldong21@gmail.com>
2019-03-14 11:16:31 +0800

    Add redux-thunk

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,11 @@
-import { createStore, combineReducers, Store, compose } from 'redux';
+import {
+    createStore,
+    combineReducers,
+    Store,
+    compose,
+    applyMiddleware,
+} from 'redux';
+import thunk from 'redux-thunk';
 import { editorReducer } from './editor/reducers';
 import { nodesReducer } from './nodes/reducers';
 
@@ -12,6 +19,10 @@ export type AppState = ReturnType<typeof rootReducer>;
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function configureStore(): Store {
-    const store = createStore(rootReducer, composeEnhancer());
+    const store = createStore(
+        rootReducer,
+        {},
+        composeEnhancer(applyMiddleware(thunk))
+    );
     return store;
 }
